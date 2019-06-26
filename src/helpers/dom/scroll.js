9a50af9172accf1772f4b57cd717f574658e4eb7
wangkaiwd <1162486176@qq.com>
2019-06-26 11:32:34 +0800

    feat: 封装水平滚动动画

---
+++ src/helpers/dom/scroll.js
@@ -0,0 +1,24 @@
+/**
+ * Created by wangkai on 2019-06-26
+ */
+
+const scrollTo = (to, element) => {
+  if (element.timerId) {
+    clearInterval(element.timerId);
+    element.timerId = null;
+    return;
+  }
+  element.timerId = null;
+  const rate = 10;
+  element.timerId = setInterval(() => {
+    const perTick = (to - element.scrollLeft) / rate;
+    // 正向考虑，perTick > 0
+    if (Math.abs(to - element.scrollLeft) < rate) {
+      clearInterval(element.timerId);
+      element.timerId = null;
+      element.scrollLeft = to;
+      return;
+    }
+    element.scrollLeft = element.scrollLeft + perTick;
+  }, 20);
+};
